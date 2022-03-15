9c1604d0139984ee8a6adba7e7e06ec132a4bd48
tomi0505 <tgdevit@gmail.com>
2022-03-15 12:17:32 +0100

    form 100% ready

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
   const prevTabInfoBelt = document.querySelector('.our-offert-nav-b__prev-tab-info-belt');
   const nextTabInfoBelt = document.querySelector('.our-offert-nav-b__next-tab-info-belt');
 
+  const customRadioEls = document.querySelectorAll('.our-offert-form-b__custom-radio');
+
+  for(const radioInput of customRadioEls) {
+    const radioInputContainerEl = radioInput.closest('.our-offert-form-b__custom-radio-container');
+
+    if(radioInput.checked) {
+      radioInputContainerEl.classList.add('our-offert-form-b__custom-radio-container--active-bg-js');
+    } else {
+      radioInputContainerEl.classList.remove('our-offert-form-b__custom-radio-container--active-bg-js');
+    }
+  }
+
+  for(const radioInputEl of customRadioEls) {
+    radioInputEl.addEventListener('change', function() {
+      for(const radioInput of customRadioEls) {
+        const radioInputContainerEl = radioInput.closest('.our-offert-form-b__custom-radio-container');
+
+        if(radioInput.checked) {
+          radioInputContainerEl.classList.add('our-offert-form-b__custom-radio-container--active-bg-js');
+        } else {
+          radioInputContainerEl.classList.remove('our-offert-form-b__custom-radio-container--active-bg-js');
+        }
+      }
+    });
+  }
+
   // init tooltips
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
